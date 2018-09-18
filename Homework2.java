68994168bce0cd7e1b90ddffff5c1698f4fa5ac5
Rajshree <chettraj@berkeleyprep.org>
2018-09-17 22:47:25 -0400

    I tried :(

--- Homework2.java
+++ Homework2.java
@@ -5,6 +5,25 @@
 import java.awt.Rectangle;
 
 public class Homework2 {
+	private String s;
+	public Homework2(String q){
+		q = s;
+	}
+
+	public String uppercase(){
+		String hob = "hello";
+			String blob = hob.toUpperCase();
+			return blob;
+
+		}
+		public String strip(String loop){
+			String s1 = "Hi! I love pineapples, trees, and hugs.";
+			String s2 = s1.replace("Hi! I love pineapples, trees, and hugs", "HiIlovepineapplestreesandhugs");
+			return s2;
+
+		}
+
+
 
 	/* Write the constructor for Homework2.
 	 * It takes in one parameter: a String
@@ -33,22 +52,26 @@ public class Homework2 {
 	/* Fix the errors in the method problem4
 	 */
 	public Rectangle problems() {
-		Rectangle r1 = (5, 10, 15, 20);
-		double width = Rectangle(5, 10, 15, 20).getWidth();
-		Rectangle r2;
+		Rectangle r1 = new Rectangle(5, 10, 15, 20);
+		double width = r1.getWidth();
+		Rectangle r2 = new Rectangle(10, 40, 8, 10);
 		r2.translate(15, 25);
-		r3 = new Rectangle();
-		r3.translate("Far, far away");
-		
+		Rectangle r3 = new Rectangle(6, 7, 8, 9);
+		r3.translate(1000000000, 1000000000);
+
 		return r3;
 	}
+
 
 
 	/* Use Rectangle methods to calculate and return the perimeter of the
 	 * parameter Rectangle
 	 */
-	public double getPerimeter(Rectangle rect) {
+	 	public double getPerimeter(Rectangle rect){
+			Rectangle oop = new Rectangle(10, 4, 5, 6);
+			double arf = 10*2 + 4*2;
+			return arf;
 
-	}
 
+		}
 }
